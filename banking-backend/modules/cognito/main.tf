cbd6358d31c2b29ed4d9f40053e6b6cb131637c9
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2025-01-15 20:46:52 -0500

    cognito added case sensitivity

--- banking-backend/modules/cognito/main.tf
+++ banking-backend/modules/cognito/main.tf
@@ -1,5 +1,12 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = "mypool"
+
+  dynamic "username_configuration" {
+    for_each = local.username_configuration.value
+    content {
+      case_sensitive = lookup(username_configuration, "case_sensitive")
+    }
+  }
 }
 
 resource "aws_cognito_user_pool_client" "user_pool_client" {
