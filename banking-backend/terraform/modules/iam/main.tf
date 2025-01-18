0f67a929c1ea2edb4799cb9029ea95bc699bc4c6
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2025-01-18 17:49:27 -0500

    iam role + attachment

--- banking-backend/terraform/modules/iam/main.tf
+++ banking-backend/terraform/modules/iam/main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy" "flask-policy" {
+resource "aws_iam_policy" "flask_cognito_policy" {
     name = "flask-cognito-policy"
 
     
@@ -14,4 +14,26 @@ resource "aws_iam_policy" "flask-policy" {
             }
         ]
     })
+}
+
+resource "aws_iam_role" "flask_cognito_role" {
+  name = "flask-cognito-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cognito-identity.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "flask_policy_attachment" {
+  role       = aws_iam_role.flask_cognito_role.name
+  policy_arn = aws_iam_policy.flask_cognito_policy.arn
 }
